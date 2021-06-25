c10f5adad1c6cf550eb2a8c79550c80d049594e1
Tao Sun <tsu@boozt.com>
2021-06-25 15:10:52 +0200

    save with old code in comment ;

---
+++ src/API/index.php
@@ -0,0 +1,12 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+const FOLDERPATH = 'upload-files/';
+$file_tmp = $_FILES['file']['tmp_name'];
+$fileNameArr = explode('.', $_FILES['file']['name']);
+$file_ext = strtolower(end($fileNameArr));
+$fiel_name = strtolower(reset($fileNameArr));
+$file = FOLDERPATH . $fiel_name . '_' . uniqid('', true) . '.' . $file_ext;
+move_uploaded_file($file_tmp, $file);
